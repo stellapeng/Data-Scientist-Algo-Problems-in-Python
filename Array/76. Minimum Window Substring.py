bc7caf2a1b8bb0f3acdab304fc5c4a7316efc760
stellapeng <37665167+stellapeng@users.noreply.github.com>
2022-03-21 13:15:02 -0400

    Update 76. Minimum Window Substring.py

--- Array/76. Minimum Window Substring.py
+++ Array/76. Minimum Window Substring.py
@@ -1,34 +1,40 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t or not s:
-            return ''
+        need = Counter(t)
+        window = defaultdict(int)
         
-        dict_t = Counter(t)
-        required = len(dict_t)
-        l,r = 0,0
-        formed = 0
-        window_counts = {}
-        ans = float('inf'),None,None
+        left, right = 0, 0
+        valid = 0
         
-        while r<len(s):
-            character = s[r]
-            window_counts[character] = window_counts.get(character,0)+1
+        length = float('inf')
+        start = 0
+        
+        while (right < len(s)):
+            # move c into the sliding window
+            c = s[right]
+            right += 1
             
-            if character in dict_t and window_counts[character] == dict_t[character]:
-                formed += 1
+            # ... UPDATE THE SLIDING WINDOW ... 
+            if c in need:
+                window[c] += 1 # or define window = {}, window[c] = window.get(c, 0) + 1
+                if window[c] == need[c]:
+                    valid += 1
             
-            while l<=r and formed==required:
-                character = s[l]
+            # if need to shrink the window
+            while (valid == len(need)):
+                # update the minimum substring
+                if right - left < length:
+                    start = left
+                    length = right - left
                 
-                if r-l+1 < ans[0]:
-                    ans = (r-l+1,l,r)
+                # moving d outside of the sliding window
+                d = s[left]
+                left += 1
+                
+                # ... UPDATE THE SLIDING WINDOW ... 
+                if d in need:           
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] -= 1
                     
-                window_counts[character] -= 1
-                if character in dict_t and window_counts[character]<dict_t[character]:
-                    formed -= 1
-                
-                l += 1
-            
-            r += 1
-        
-        return '' if ans[0]==float('inf') else s[ans[1]:ans[2]+1]
+        return '' if length == float('inf') else s[start:(start+length)]

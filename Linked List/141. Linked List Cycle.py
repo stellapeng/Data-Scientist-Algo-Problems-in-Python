f89c644424f214ca22822dd28cd9bee28666022b
stellapeng <37665167+stellapeng@users.noreply.github.com>
2022-03-26 22:51:51 -0400

    Create 141. Linked List Cycle.py

---
+++ Linked List/141. Linked List Cycle.py
@@ -0,0 +1,23 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        
+        while (fast != None and fast.next != None):
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
+                return True
+        
+        return False
+        
+        

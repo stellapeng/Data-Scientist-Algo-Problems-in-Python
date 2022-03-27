2460663c85e44c0b3d407ee80afb5a67aff9fac6
stellapeng <37665167+stellapeng@users.noreply.github.com>
2022-03-26 22:38:45 -0400

    Create 876. Middle of the Linked List.py

---
+++ Linked List/876. Middle of the Linked List.py
@@ -0,0 +1,19 @@
+# If there are two middle nodes, return the second middle node.
+# Time Complexity: O(n), one pass, n
+# Space Complexity: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        
+        while (fast is not None and fast.next is not None):
+            slow = slow.next
+            fast = fast.next.next
+        
+        return slow
+

a4e6c6fb06ffb6608c92c6efc07af9d39505eb82
stellapeng <37665167+stellapeng@users.noreply.github.com>
2022-04-27 23:43:29 -0400

    Create 224. Basic Calculator.py

---
+++ Others/224. Basic Calculator.py
@@ -0,0 +1,35 @@
+class Solution:
+    def helper(self, s):
+        sign = '+'
+        stk = []
+        num = 0
+        while len(s) > 0:
+            c = s.popleft()
+            if c.isdigit():
+                num = num * 10 + int(c)
+            
+            if c == '(':
+                num = self.helper(s)
+                
+            if (not c.isdigit() and c != ' ') or (len(s)==0):
+                if sign == '+':
+                    stk.append(num)
+                elif sign == '-':
+                    stk.append(-num)
+                elif sign == '*':
+                    stk[-1] = stk[-1] * num
+                elif sign == '/':
+                    stk[-1] = int(stk[-1] / float(num))                    
+                num = 0
+                sign = c
+                
+            if c == ')': 
+                break
+
+        return sum(stk)
+                    
+    
+    def calculate(self, s: str) -> int:
+        return self.helper(collections.deque(s))
+        
+   

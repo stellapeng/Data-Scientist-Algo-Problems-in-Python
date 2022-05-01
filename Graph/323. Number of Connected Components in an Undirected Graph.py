7d9fde9004f9711bedd03bc6e6c075a012d1db61
stellapeng <37665167+stellapeng@users.noreply.github.com>
2022-04-30 22:54:58 -0400

    Create 323. Number of Connected Components in an Undirected Graph.py

---
+++ Graph/323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,39 @@
+# Time Complexisity: union and find are O(1)
+
+class UF:
+    def __init__(self, n):
+        self.count = n
+        self.parent = [0] * n
+        for i in range(n):
+            self.parent[i] = i
+    
+    def union(self, p, q):
+        rootP = self.find(p)
+        rootQ = self.find(q)
+        
+        if (rootP == rootQ):
+            return
+        
+        self.parent[rootQ] = rootP
+        self.count -= 1
+        
+    def connected(self, p, q):
+        rootP = self.find(p)
+        rootQ = self.find(q)
+        return rootP == rootQ
+
+    def find(self, x):
+        if (self.parent[x] != x):
+            # compress the depth
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UF(n)
+        for e in edges:
+            uf.union(e[0], e[1])
+        return uf.count
+            
+        
